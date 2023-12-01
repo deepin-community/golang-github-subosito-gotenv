b1ff0ab3576b2797740e76174b2bfe28694e17cb
李成刚 <lichenggang@uniontech.com>
2023-12-01 16:41:37 +0800

    feat: update golang-github-subosito-gotenv to 1.3.0

--- gotenv_test.go
+++ gotenv_test.go
@@ -127,6 +127,9 @@ var formats = []struct {
 
 	// allows # in quoted value with spaces after separator
 	{`foo= "bar#baz" # comment`, gotenv.Env{"foo": "bar#baz"}, false},
+
+	// allows = in double quoted values with newlines (typically base64 padding)
+	{`foo="---\na==\n---"`, gotenv.Env{"foo": "---\na==\n---"}, false},
 }
 
 var errorFormats = []struct {
@@ -150,6 +153,27 @@ var fixtures = []struct {
 		gotenv.Env{
 			"OPTION_A": "2",
 			"OPTION_B": `\n`,
+			"OPTION_C": `The MIT License (MIT)
+
+Copyright (c) 2013 Alif Rachmawadi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.`,
 		},
 	},
 	{
@@ -173,6 +197,9 @@ var fixtures = []struct {
 			"OPTION_F": "2",
 			"OPTION_G": "",
 			"OPTION_H": "\n",
+			"OPTION_I": `some multi-line text
+with "escaped quotes" and 1 variable`,
+			"OPTION_J": `some$pecial$1$2!*chars=qweq""e$$\$""`,
 		},
 	},
 	{
@@ -237,6 +264,22 @@ func TestLoad_overriding(t *testing.T) {
 	err := gotenv.Load()
 	assert.Nil(t, err)
 	assert.Equal(t, v, os.Getenv(k))
+	os.Clearenv()
+}
+
+func TestLoad_overrideVars(t *testing.T) {
+	os.Setenv("A", "fromEnv")
+	err := gotenv.Load("fixtures/vars.env")
+	assert.Nil(t, err)
+	assert.Equal(t, "fromEnv", os.Getenv("B"))
+	os.Clearenv()
+}
+
+func TestLoad_overrideVars2(t *testing.T) {
+	os.Setenv("C", "fromEnv")
+	err := gotenv.Load("fixtures/vars.env")
+	assert.Nil(t, err)
+	assert.Equal(t, "fromEnv", os.Getenv("D"))
 	os.Clearenv()
 }
 
@@ -320,6 +363,24 @@ func TestOverLoad_overriding(t *testing.T) {
 	err := gotenv.OverLoad()
 	assert.Nil(t, err)
 	assert.Equal(t, "world", os.Getenv(k))
+	os.Clearenv()
+}
+
+func TestOverLoad_overrideVars(t *testing.T) {
+	os.Setenv("A", "fromEnv")
+	err := gotenv.OverLoad("fixtures/vars.env")
+	assert.Nil(t, err)
+	assert.Equal(t, "fromFile", os.Getenv("B"))
+	os.Clearenv()
+}
+
+func TestOverLoad_overrideVars2(t *testing.T) {
+	os.Setenv("C", "fromEnv")
+	err := gotenv.OverLoad("fixtures/vars.env")
+	assert.Nil(t, err)
+	// The value for D is not "fromFile" because C is defined after the
+	// definition of D.
+	assert.Equal(t, "fromEnv", os.Getenv("D"), "C defined after usage")
 	os.Clearenv()
 }
 
